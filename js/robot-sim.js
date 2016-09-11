550a972e966e8e84870350e9edf06510f8b036b9
Dean Pillay <ddpillay26@gmail.com>
2016-09-11 22:15:32 +0200

    Changes to CSS, JS and homepage

--- js/robot-sim.js
+++ js/robot-sim.js
@@ -1,12 +1,73 @@
-var robot       = {},
-    currentPos  = {},
-    errorMsg    = document.getElementById('error'),
-    reportMsg   = document.getElementById('report'),
-    direction   = ['NORTH','SOUTH', 'EAST', 'WEST'],
-    canvas      = document.getElementById('canvas'),
-    ctx         = canvas.getContext('2d') ;
+window.onload = createCanvas;
 
-function validateX(axis) {
+var robot;
+var currentPos  = {},
+var errorMsg    = document.getElementById("error");
+var reportMsg   = document.getElementById("report");
+var direction   = ["NORTH", "SOUTH", "EAST", "WEST"];
+var canvas      = document.getElementById("canvas1");
+var ctx         = canvas.getContext("2d");
+
+function createCanvas() {
+    ctx.beginPath();
+	ctx.moveTo(80,0);
+	ctx.lineTo(80,0);
+	ctx.lineTo(80,400);
+    ctx.strokeStyle = "#eee";
+	ctx.stroke();
+
+	ctx.beginPath();
+	ctx.moveTo(160,0);
+	ctx.lineTo(160,0);
+	ctx.lineTo(160,400);
+    ctx.strokeStyle = "#eee";
+	ctx.stroke();
+
+	ctx.beginPath();
+	ctx.moveTo(240,0);
+	ctx.lineTo(240,0);
+	ctx.lineTo(240,400);
+    ctx.strokeStyle = "#eee";
+	ctx.stroke();
+
+	ctx.beginPath();
+	ctx.moveTo(320,0);
+	ctx.lineTo(320,0);
+	ctx.lineTo(320,400);
+    ctx.strokeStyle = "#eee";
+	ctx.stroke();
+
+	//Draw the horizontal lines
+	ctx.beginPath();
+	ctx.moveTo(0,80);
+	ctx.lineTo(0,80);
+	ctx.lineTo(400,80);
+    ctx.strokeStyle = "#eee";
+	ctx.stroke();
+
+	ctx.beginPath();
+	ctx.moveTo(0,160);
+	ctx.lineTo(0,160);
+	ctx.lineTo(400,160);
+    ctx.strokeStyle = "#eee";
+	ctx.stroke();
+
+	ctx.beginPath();
+	ctx.moveTo(0,240);
+	ctx.lineTo(0,240);
+	ctx.lineTo(400,240);
+    ctx.strokeStyle = "#eee";
+	ctx.stroke();
+
+	ctx.beginPath();
+	ctx.moveTo(0,320);
+	ctx.lineTo(0,320);
+	ctx.lineTo(400,320);
+    ctx.strokeStyle = "#eee";
+	ctx.stroke();
+}
+
+function validateX(axisX) {
     if (isNaN(axis)) {
         errorMsg.innerHTML= "Please enter in numberic co-ordinates";
     } else if (axis < 0 || axis > 4 ) {
@@ -16,7 +77,7 @@ function validateX(axis) {
     }
 }     
 
-function validateY(axis) {
+function validateY(axisY) {
     if (isNaN(axis)) {
         errorMsg.innerHTML("Please enter in numberic co-ordinates");
     } else if (axis < 0 || axis > 4 ) {
@@ -80,7 +141,7 @@ function move() {
     }
 }
 
-function processCommand(command) {
+function processCmd(command) {
     errorMsg.innerHTML = "";
     currentPos = {
         x: robot.x,
@@ -91,3 +152,14 @@ function processCommand(command) {
     var commandFirst   = commandEntered[0].toUpperCase();
     
 }
+
+function createRobot(rob) {
+
+}
+
+function rotateRobot(directions) {
+    if (directions === "LEFT") {
+
+    }
+}
+
